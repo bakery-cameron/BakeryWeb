9b383b6f82b41f9a11ace3f1f8180e928af67ced
bakery-cameron <cmiller@bakeryinteractive.com>
2015-09-15 14:36:38 -0500

    email on contact added

--- server/index.js
+++ server/index.js
@@ -2,6 +2,21 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var fs = require('fs');
+//-----email stuff-------//
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
+
+var MG_API_KEY = process.env.MG_API_KEY;
+var MG_DOMAIN = 'sandbox839634cef75d498590b21309c025332c.mailgun.org';
+if(!MG_API_KEY) console.log('No Mailgun API KEY in env vars');
+var mailgun = require('mailgun-js')({apiKey:MG_API_KEY,domain:MG_DOMAIN});
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+//-----------------------//
+
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -38,6 +53,74 @@ app.get('*', function(req, res) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
+
+//-----email stuff-------//
+
+
+app.post('/email-message',function(req,res){
+	var data = {
+		from: req.body.email,
+		to: 'cmiller060@gmail.com',
+		subject: 'Message From Bakery.agency',
+		text: req.body.text
+	};
+	mailgun.messages().send(data, function (err, body) {
+		if (err){
+			console.log('Oh noes: ' + err);
+			res.json({ok:false});
+		} else {
+			console.log('Success', body);
+			res.json({ok:true});
+		}
+	});
+});
+app.post('/email-project',function(req,res){
+	console.log(req.body)
+	var data = {
+		from: req.body.email,
+		to: 'cmiller060@gmail.com',
+		subject: 'Project From Bakery.agency',
+		text: req.body.text
+	};
+	mailgun.messages().send(data, function (err, body) {
+		if (err){
+			console.log('Oh noes: ' + err);
+			res.json({ok:false});
+		} else {
+			console.log('Success', body);
+			res.json({ok:true});
+		}
+	});
+});
+app.post('/email-resume',upload.single('resume'),function(req,res){
+	var attch = new mailgun.Attachment({data: req.file.buffer, filename: req.file.originalname});
+	var data = {
+		from: req.body.email,
+		to: 'cmiller060@gmail.com',
+		subject: 'Resume From Bakery.agency',
+		text: req.body.text,
+		attachment: attch
+	};
+	mailgun.messages().send(data, function (err, body) {
+		if (err){
+			console.log('Oh noes: ' + err);
+			res.json({ok:false});
+		} else {
+			console.log('Success', body);
+			res.json({ok:true});
+		}
+	});
+});
+
+
+
+
+//-----------------------//
+
+
+
 
 function isValidFolder(folder){
 	stats = fs.lstatSync(path.join(__dirname , folder));
